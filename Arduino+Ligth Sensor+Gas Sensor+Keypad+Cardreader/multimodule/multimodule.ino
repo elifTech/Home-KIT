2252baff1c1e7bbc3a63d5a5cbe91950f74d0826
dmytro <dmytro.kucheryavenko@eliftech.com>
2016-10-20 13:23:07 +0300

    Revert "add new example"
    This reverts commit c8c16d22c8e8fd3bf5d289234a9e5a63c9cda7d7.

--- Arduino+Ligth Sensor+Gas Sensor+Keypad+Cardreader/multimodule/multimodule.ino
+++ Arduino+Ligth Sensor+Gas Sensor+Keypad+Cardreader/multimodule/multimodule.ino
@@ -1,55 +1,24 @@
+#include <SPI.h>
+#include <MFRC522.h>
 #include <Thread.h>
 #include <ThreadController.h>
 #include <Keypad.h>
-#include "dht.h"
-#include "UIPEthernet.h"
-#include "PubSubClient.h"
 
-EthernetClient ethClient;
-PubSubClient client;
+#define RST_PIN         10         // Configurable, see typical pin layout above
+#define SS_PIN          53         // Configurable, see typical pin layout above
 
-byte mac[]    = {  0x00, 0x01, 0x02, 0x03, 0x04, 0x05D };
-byte server[] = { 192, 168, 0, 33 };
-
-
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.println("callback");
-}
-void startEthernet() {
-  Serial.println("start Ethernet");
-  Ethernet.begin(mac); //start ethernet
-}
-void startMqtt() {
-  client.setClient(ethClient); //set client for mqqt (ethernet)
-  client.setServer(server, 1883); // set server for mqqt(raspberry)
-  client.setCallback(callback); // set callback for subscripting
-
-  client.connect("arduino"); //connect as arduino
-  if (!client.connected()) {
-    while (!client.connected()) {
-      Serial.print(".");
-      client.connect("arduino");
-    }
-  }
-  Serial.println("Connected to MQTT server");
-}
-void subscripting() {
-  client.subscribe("status");
-}
 
 // ThreadController that will controll all threads
 ThreadController controll = ThreadController();
 Thread gasSensorThread = Thread();
+Thread cardReaderThread = Thread();
 Thread photocellThread = Thread();
 Thread keypadThread = Thread();
-Thread meteoThread = Thread();
 
 const int gasPin = A0;
 const int photocellPin = A1;
 const byte numRows = 4; //number of rows on the keypad
 const byte numCols = 4; //number of columns on the keypad
-dht DHT;
-#define DHT11_PIN 30
 
 char keymap[numRows][numCols] =
 {
@@ -67,9 +36,33 @@ Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols)
 
 int photocellReading;
 
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+String UIDstring;
+
 void gasSensorCallback() {
   Serial.println("Gas sensor says:");
   Serial.println(analogRead(gasPin));
+}
+
+void cardReaderCallback() {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+  Serial.println("Card id:");
+  UIDstring = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    UIDstring = UIDstring + String(mfrc522.uid.uidByte[i]);
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  }
+  Serial.println();
 }
 
 void photocellCallback() {
@@ -86,48 +79,32 @@ void keypadCallback() {
   }
 }
 
-void meteoCallback() {
-  int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
-}
 void setup() {
   Serial.begin(9600);   // Initialize serial communications with the PC
-
-  //  startEthernet();
-  // startMqtt();
-  //  subscripting();
-
   gasSensorThread.onRun(gasSensorCallback);
   gasSensorThread.setInterval(1000);
 
+  cardReaderThread.onRun(cardReaderCallback);
+  cardReaderThread.setInterval(0);
+
   photocellThread.onRun(photocellCallback);
-  photocellThread.setInterval(1000);
+  photocellThread.setInterval(2000);
 
   keypadThread.onRun(keypadCallback);
   keypadThread.setInterval(0);
 
-  meteoThread.onRun(meteoCallback);
-  meteoThread.setInterval(1000);
-
   controll.add(&gasSensorThread);
+  controll.add(&cardReaderThread);
   controll.add(&photocellThread);
   controll.add(&keypadThread);
-  controll.add(&meteoThread);
+
+  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  Serial.println(F("Scan PICC to see UID, type, and data blocks..."));
 }
 
 void loop() {
   controll.run();
-  //    if (!client.connected())
-  //  {
-  //    // clientID, username, MD5 encoded password
-  //    client.connect("arduino");
-  //    subscripting();
-  //    client.loop();
-  //  }
-  //
-  //  // MQTT client loop processing
-  //  client.loop();
 }
